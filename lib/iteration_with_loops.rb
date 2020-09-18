adf27c06e0c8c71d59f0abc75922e4e18eb29bcb
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-18 19:56:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,13 @@
 def join_nested_strings(src)
+  string_array = []
   for outer in src do
-    
     for inner in outer do
-      if inner
+      if inner.is_a?(String) then
+        string_array.sub(inner)
+      end
     end
     
   end
+  
+  string_array
 end
